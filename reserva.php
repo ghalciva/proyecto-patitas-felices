0f549ce311871c03ceb01e19a675961c22b7563c
Andrea Jurado <avjh_18@hotmail.com>
2017-08-20 18:47:45 -0500

    Clase reserva php

---
+++ reserva.php
@@ -0,0 +1,41 @@
+<?php
+
+class reserva
+{
+    private $id_reserva;
+    private $fecha_reserva;
+    private $direccion;
+   
+    
+     function __construct($id_reserva, $fecha_reserva, $direccion) {
+       $this->id_reserva = $id_reserva;
+       $this->fecha_reserva = $fecha_reserva;
+       $this->direccion = $direccion;
+      
+     }
+    
+     function setId_Reserva($id_reserva){
+       $this->id_reserva = $id_reserva;
+     } 
+     function getId_Reserva(){
+       return $this->id_reserva;
+     } 
+     function setFecha_Reserva($fecha_reserva){
+       $this->fecha_reserva = $fecha_reserva;
+     } 
+     function getFecha_Reserva(){
+       return $this->fecha_reserva;
+     } 
+    
+ 	function setDireccion($direccion){
+       $this->direccion = $direccion;
+     } 
+     function getDireccion(){
+       return $this->direccion;
+     } 
+    
+
+
+}
+
+?> 
