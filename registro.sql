2a3421a2967d6dad0041229bd9efaa92a12b8f80
jesangai <jagg1996.jj@gmail.com>
2017-08-21 00:51:43 -0500

    registro sql

---
+++ registro.sql
@@ -0,0 +1,13 @@
+﻿SELECT 
+  persona.id_persona, 
+  persona.nombre, 
+  persona.apellido, 
+  persona."contraseña", 
+  usuario.id_usuario, 
+  usuario.id_persona, 
+  usuario."contraseña_user"
+FROM 
+  public.persona, 
+  public.usuario
+WHERE 
+  persona.id_persona = usuario.id_persona;
