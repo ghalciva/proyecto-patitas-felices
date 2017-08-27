67da71c549f95c847fc157a6c367995d4d207626
Andrea Jurado <avjh_18@hotmail.com>
2017-08-27 18:16:57 -0500

    Veterinario modificado

--- veterinario.php
+++ veterinario.php
@@ -10,24 +10,42 @@ class veterinario
    
     
      function __construct($id_vet, $id_mascota,$nombre_vet, $nombre_veterinaria, $direccion) {
-       $this->id_reserva = $id_reserva;
-       $this->fecha_reserva = $fecha_reserva;
+       $this->id_vet = $id_vet;
+       $this->id_mascota = $id_mascota;
+	$this->nombre_vet = $nombre_vet;
+	$this->nombre_veterinaria = $nombre_veterinaria;
        $this->direccion = $direccion;
+	
       
      }
     
-     function setId_Reserva($id_reserva){
-       $this->id_reserva = $id_reserva;
+     function setId_Vet($id_vet){
+       $this->id_vet = $id_vet;
      } 
-     function getId_Reserva(){
-       return $this->id_reserva;
+     function getId_Vet(){
+       return $this->id_vet;
      } 
-     function setFecha_Reserva($fecha_reserva){
-       $this->fecha_reserva = $fecha_reserva;
+     function setId_Mascota($id_mascota){
+       $this->id_mascota = $id_mascota;
      } 
-     function getFecha_Reserva(){
-       return $this->fecha_reserva;
+	function getId_Mascota(){
+       return $this->id_mascota;
      } 
+
+function setNombre_Vet($nombre_vet){
+       $this->nombre_vet = $nombre_vet;
+     } 
+     function getNombre_Vet(){
+       return $this->nombre_vet;
+     }
+
+	function setNombre_Veterinaria($nombre_veterinaria){
+       $this->nombre_veterinaria = $nombre_veterinaria;
+     } 
+     function getNombre_Veterinaria(){
+       return $this->nombre_veterinaria;
+     } 
+ 
     
  	function setDireccion($direccion){
        $this->direccion = $direccion;
