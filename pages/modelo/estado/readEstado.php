504d4775a61faa589a7e781a8012b6f8def24811
Andrea Jurado <avjh_18@hotmail.com>
2017-09-17 12:58:20 -0500

    Tabla adopcion corregida

--- pages/modelo/estado/readEstado.php
+++ pages/modelo/estado/readEstado.php
@@ -59,7 +59,7 @@ $estadoCollectorObj = new estadoCollector();
 <span class="icon-bar"></span>
 </button>
 <a class="navbar-brand" href="index.php">
-<img src="img/logo.png" alt="Logo">
+<img src="../../../img/logo.png" alt="Logo">
 </a>
 </div>
 
