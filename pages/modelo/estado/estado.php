7bdd272a75711008f532a76fed733427895e6725
Andrea Jurado <avjh_18@hotmail.com>
2017-09-17 12:50:44 -0500

    Tabla estado adopción corregido

--- pages/modelo/estado/estado.php
+++ pages/modelo/estado/estado.php
@@ -8,37 +8,30 @@ class estado
     
      function __construct($id_estado,$descripcion) {
 
- 	$this->id_estado = $id_estado;
+ 	    $this->id_estado = $id_estado;
        $this->descripcion = $descripcion;
      }
 
-<<<<<<< HEAD
+
     function setIdEstado($id_estado){
        $this->id_estado = $id_estado;
      } 
+
+
      function getIdEstado(){
        return $this->id_estado;
      } 
 
-function setDescripcion($descripcion){
+
+      function setDescripcion($descripcion){
        $this->descripcion = $descripcion;
-     } 
-     function getDescripcion(){
-=======
-    function setid_estado($id_estado){
-       $this->id_estado = $id_estado;
-     } 
-     function getid_estado(){
-       return $this->id_estado;
      } 
 
-function setdescripcion($descripcion){
-       $this->descripcion = $descripcion;
-     } 
-     function getdescripcion(){
->>>>>>> c3d28f6c9be7dcc91186c9a048e297e1ca51aa62
+      function getDescripcion(){
        return $this->descripcion;
-     } 
+      } 
+
+      
 }
 
 ?>
