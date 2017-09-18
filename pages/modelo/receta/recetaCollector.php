c5cee8cf1c638f59574673a2abeaedd75724a46a
Andrea Jurado <avjh_18@hotmail.com>
2017-09-18 15:47:29 -0500

    Modificación

--- pages/modelo/receta/recetaCollector.php
+++ pages/modelo/receta/recetaCollector.php
@@ -23,7 +23,7 @@ class recetaCollector extends Collector
  }
 
 function updateReceta($id_receta, $id_consulta, $descripcion, $cantidad) {
-   $insertrow = self::$db->updateRow("UPDATE public.receta SET descripcion = ?, cantidad = ? WHERE id_receta= ? " , array("{$id_receta}", "{$id_consulta}", "{$descripcion}", "{$cantidad}"));  
+   $insertrow = self::$db->updateRow("UPDATE public.receta SET descripcion = ?, cantidad = ? WHERE id_receta= ?, id_consulta=? " , array("{$id_receta}", "{$id_consulta}", "{$descripcion}", "{$cantidad}"));  
  }
 
 
